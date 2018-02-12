1d0f439212055f8cf7a3bc11dd7291e2a3e93728
Rikud <ivan_nemshilov@mail.ru>
2018-02-12 02:20:09 -0800

    Добавил стрелочные функции

--- source/minmax.js
+++ source/minmax.js
@@ -1,13 +1,13 @@
 const min = numbers => Math.min(...numbers);
 
-const minmax = function (numbers) {
+const minmax = (numbers) => {
 	let arr = parser(numbers);
 	if (arr.length === 0)
 		return [undefined, undefined];
 	return minmax_array = [min(arr), max(arr)];
 }
 
-const parser = function (input) {
+const parser = (input) => {
 	if (typeof input !== 'string')
 		return [];
 	let arr = input.split(/,|\s/);
